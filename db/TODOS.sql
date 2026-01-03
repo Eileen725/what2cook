714a31ed3d9a00692feedff15b68e86abf4afbe5
Eileen Seiler <eileen.seiler@hotmail.com>
2026-01-03 18:13:25 +0100

    rezepte sql angepasst

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -17,28 +17,28 @@ CREATE TABLE zutaten (
     rezept_id INT NOT NULL,
     name VARCHAR(250) NOT NULL,
     number INT,
-    einheit VARCHAR(50),
+    einheit VARCHAR(50);
     FOREIGN KEY (rezept_id) REFERENCES rezepte(id)
 );
 
 -- Beschreibungen von jedem Rezept (kann man noch ändern)
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Pizza', 'Klassischer Pizzateig mit Tomatensauce und Käse');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Spaghetti', 'Spaghetti mit Butter - Kindheistklassiker');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Tomatensauce', 'Frische Tomatensauce von scratch');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'toms Erdbeermarmeladenbrot mit Honig', 'Kindheitsklassiker');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Cinque Pi', 'Cremige Pasta mit Rahm-Tomatensauce');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Armer Ritter', 'Einfaches süsses Frühstück');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Bruschetta', 'Klassische italienische Häpchenkost');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'tortilla de patatas', 'Traditioneller spanischer Klassiker');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Bratkartoffeln', 'Knusprige Kartoffelwürfel, perfekt als Beilage');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Pizza', 'Klassischer Pizzateig mit Tomatensauce und Käse');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Spaghetti', 'Spaghetti mit Butter - Kindheistklassiker');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Tomatensauce', 'Frische Tomatensauce von scratch');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'toms Erdbeermarmeladenbrot mit Honig', 'Kindheitsklassiker');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Cinque Pi', 'Cremige Pasta mit Rahm-Tomatensauce');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Armer Ritter', 'Einfaches süsses Frühstück');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Bruschetta', 'Klassische italienische Häpchenkost');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'tortilla de patatas', 'Traditioneller spanischer Klassiker');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Bratkartoffeln', 'Knusprige Kartoffelwürfel, perfekt als Beilage');
 
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Spaghetti Bolognese', 'Klassische italienische Pasta mit Hackfleischsauce');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Hähnchen-Curry', 'Cremiges indisches Curry mit zartem Hähnchenfleisch');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Caesar Salad', 'Frischer Salat mit Parmesan und knusprigen Croutons');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Tomatensuppe', 'Cremige Suppe aus frischen Tomaten');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Pfannkuchen', 'Fluffige amerikanische Pancakes zum Frühstück');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Guacamole', 'Mexikanischer Avocado-Dip');
-INSERT INTO rezepte (user_id, name, description) VALUES (1, 'Rührei', 'Einfaches Frühstück mit Eiern');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Spaghetti Bolognese', 'Klassische italienische Pasta mit Hackfleischsauce');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Hähnchen-Curry', 'Cremiges indisches Curry mit zartem Hähnchenfleisch');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Caesar Salad', 'Frischer Salat mit Parmesan und knusprigen Croutons');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Tomatensuppe', 'Cremige Suppe aus frischen Tomaten');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Pfannkuchen', 'Fluffige amerikanische Pancakes zum Frühstück');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Guacamole', 'Mexikanischer Avocado-Dip');
+INSERT INTO rezepte (user_id, name, description) VALUES (NULL, 'Rührei', 'Einfaches Frühstück mit Eiern');
 
 -- Zutaten für Pizza (rezept_id 1)
 INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (1, 'Mehl', 400, 'g');
@@ -113,60 +113,60 @@ INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (9, 'Salz', NULL, 
 --Neue Rezepte 27.12.2025
 
 -- Zutaten für Spaghetti Bolognese (rezept_id 10)
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Spaghetti', 500, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Hackfleisch', 400, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Tomaten', 800, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Zwiebeln', 2, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Knoblauch', 3, 'Zehen'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Olivenöl', 3, 'EL'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Salz', NULL, NULL),
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Spaghetti', 500, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Hackfleisch', 400, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Tomaten', 800, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Zwiebeln', 2, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Knoblauch', 3, 'Zehen');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Olivenöl', 3, 'EL');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Salz', NULL, NULL);
 INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (10, 'Pfeffer', NULL, NULL);
 
 -- Zutaten für Hähnchen-Curry (rezept_id 11)
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Hähnchenbrustfilet', 600, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Kokosmilch', 400, 'ml'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Currypaste', 2, 'EL'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Zwiebeln', 1, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Paprika', 1, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Reis', 300, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Ingwer', 1, 'Stück'),
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Hähnchenbrustfilet', 600, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Kokosmilch', 400, 'ml');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Currypaste', 2, 'EL');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Zwiebeln', 1, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Paprika', 1, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Reis', 300, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Ingwer', 1, 'Stück');
 INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (11, 'Koriander', NULL, NULL);
 
 -- Zutaten für Caesar Salad (rezept_id 12)
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Römersalat', 1, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Parmesan', 80, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Croutons', 100, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Hähnchenbrustfilet', 300, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Caesar Dressing', 100, 'ml'),
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Römersalat', 1, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Parmesan', 80, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Croutons', 100, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Hähnchenbrustfilet', 300, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Caesar Dressing', 100, 'ml');
 INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (12, 'Zitrone', 1, 'Stück');
 
 -- Zutaten für Tomatensuppe (rezept_id 13)
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Tomaten', 1000, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Zwiebeln', 1, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Knoblauch', 2, 'Zehen'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Gemüsebrühe', 500, 'ml'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Sahne', 100, 'ml'),
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Tomaten', 1000, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Zwiebeln', 1, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Knoblauch', 2, 'Zehen');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Gemüsebrühe', 500, 'ml');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Sahne', 100, 'ml');
 INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (13, 'Basilikum', NULL, NULL);
 
 -- Zutaten für Pfannkuchen (rezept_id 14)
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Mehl', 300, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Milch', 400, 'ml'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Eier', 3, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Zucker', 2, 'EL'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Backpulver', 1, 'TL'),
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Mehl', 300, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Milch', 400, 'ml');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Eier', 3, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Zucker', 2, 'EL');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Backpulver', 1, 'TL');
 INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (14, 'Butter', 50, 'g');
 
 -- Zutaten für Guacamole (rezept_id 15)
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Avocado', 3, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Tomaten', 2, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Zwiebeln', 1, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Limette', 1, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Koriander', NULL, NULL),
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Avocado', 3, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Tomaten', 2, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Zwiebeln', 1, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Limette', 1, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Koriander', NULL, NULL);
 INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (15, 'Salz', NULL, NULL);
 
 -- Zutaten für Rührei (rezept_id 16)
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (16, 'Eier', 4, 'Stück'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (16, 'Milch', 50, 'ml'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (16, 'Butter', 20, 'g'),
-INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (16, 'Salz', NULL, NULL),
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (16, 'Eier', 4, 'Stück');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (16, 'Milch', 50, 'ml');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (16, 'Butter', 20, 'g');
+INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (16, 'Salz', NULL, NULL);
 INSERT INTO zutaten (rezept_id, name, number, einheit) VALUES (16, 'Pfeffer', NULL, NULL);
